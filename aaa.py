2597b20d2eda387545de2bea14c1ddc8f42b6a3f
297098110 <297098110@users.noreply.github.com>
2018-06-22 22:45:19 +0800

    2017
    2017

--- aaa.py
+++ aaa.py
@@ -8,7 +8,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+	
+@app.route("/1")
+	return "长的好看就能随便看了啊！"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)

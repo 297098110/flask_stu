15092faa2a316eb0ab03a47d0042c3725c6a05b1
297098110 <297098110@users.noreply.github.com>
2018-06-22 22:53:48 +0800

    2017
    2017

--- aaa.py
+++ aaa.py
@@ -10,8 +10,9 @@ def index():
     return render_template("index.html")
 	
 @app.route("/1")
+def index2():
 	return "长的好看就能随便看了啊！"
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(host="45.32.34.139", port="5055",debug=False)

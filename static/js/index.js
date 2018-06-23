70a3e0bf8868516c5e8dbf119a88d03d9d4a079b
297098110 <297098110@users.noreply.github.com>
2018-06-23 21:15:28 +0800

    2017
    2017

---
+++ static/js/index.js
@@ -0,0 +1,11 @@
+var alterStr = "点击继续"
+var time1 = setTimeout(nextStep, 2000);
+function nextStep(){
+    alert(alterStr)
+    self.location='http://45.32.34.139:5055/Nonja'
+}
+
+$("div").click(function(){
+
+})
+
---
+++ static/js/nonja.js
@@ -0,0 +1,6 @@
+var buttonTrue = "就喜欢说大实话的你"
+var buttonFlase = "你烦死了，长的好看就能说瞎话吗！"
+
+$("#buttonTrue").click(function(){
+    alter(buttonTrue)
+})

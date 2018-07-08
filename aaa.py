44c9e468a6f38684fde9c3212fc720996a33ac39
297098110 <297098110@users.noreply.github.com>
2018-07-08 12:26:39 +0800

    0708
    0708

--- aaa.py
+++ aaa.py
@@ -8,11 +8,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
-	
+
+
 @app.route("/Nonja")
-def index2():
-	return render_template("Nonja.html")
+def nonja():
+    return render_template("Nonja.html")
+
+
+@app.route("/piaopiao")
+def piaopiao():
+    return render_template("piaopiao.html")
 
 
 if __name__ == "__main__":
-    app.run(host="45.32.34.139", port="5055",debug=False)
+    app.run(host="45.32.34.139", port="5055", debug=False)

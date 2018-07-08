918d1eb95bc83ad1656a762d344e9f58df6f9b9a
297098110 <297098110@users.noreply.github.com>
2018-07-08 12:39:06 +0800

    0708
    0708

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@ var alterStr = "点击继续"
 var time1 = setTimeout(nextStep, 2000);
 function nextStep(){
     alert(alterStr)
-    self.location='http://45.32.34.139:5055/Nonja'
+    self.location='http://45.32.34.139:5055/piaopiao'
 }
 
 $("div").click(function(){
---
+++ static/js/piaopiao.js
@@ -0,0 +1,6 @@
+var buttonTrue = "我看你是吃了娃娃的屎咯"
+var buttonFlase = "没吃还不赶紧吃去！"
+
+$("#buttonTrue").click(function(){
+    alter(buttonTrue)
+})
